5cdfba6ec200df751b6bcda12c1ddc5a1442a9ac
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-04-11 10:18:02 -0700

    Update Otto_models.h

--- src/Otto_models.h
+++ src/Otto_models.h
@@ -3,10 +3,8 @@
 #ifndef Otto_models_h
 #define Otto_models_h
 
-#define BIPED     1
-#define WHEELS    2
-#define NINJA     3
-#define HUMANOID  4
-#define QUAD      5
+#define BIPED     0
+#define WHEELS    1
+#define NINJA     2
 
 #endif
