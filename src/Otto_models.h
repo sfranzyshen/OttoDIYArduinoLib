599be8bdb70ae79b0944df2b74745cc60b968a4f
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-04-12 16:49:47 -0700

    Update Otto_models.h

--- src/Otto_models.h
+++ src/Otto_models.h
@@ -1,10 +1,13 @@
 // OttoDIY Arduino Library project 2024
 
 #ifndef Otto_models_h
-
 #define Otto_models_h
-#define BIPED     0
-#define WHEELS    1
-#define NINJA     2
+
+#define BIPED       0
+#define BIPED_ARMS  1
+#define WHEELS      2
+#define WHEELS_ARMS 3
+#define NINJA       4
+#define NINJA_ARMS  5
 
 #endif
