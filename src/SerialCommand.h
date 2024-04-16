d86375ac5e8fc55c1f122773b1da42c8450f224c
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-04-16 14:58:24 -0700

    Update SerialCommand.h

--- src/SerialCommand.h
+++ src/SerialCommand.h
@@ -1,39 +1,7 @@
-/******************************************************************************* 
-SerialCommand - An Arduino library to tokenize and parse commands received over
-a serial port. 
-Copyright (C) 2011-2013 Steven Cogswell  <steven.cogswell@gmail.com>
-http://awtfy.com
+// OttoDIY Arduino Library project 2024
 
-Version 20131021A.   
+// SerialCommand (C) 2011-2013 Steven Cogswell
 
-Version History:
-May 11 2011 - Initial version
-May 13 2011 -	Prevent overwriting bounds of SerialCommandCallback[] array in addCommand()
-			defaultHandler() for non-matching commands
-Mar 2012 - Some const char * changes to make compiler happier about deprecated warnings.  
-           Arduino 1.0 compatibility (Arduino.h header) 
-Oct 2013 - SerialCommand object can be created using a SoftwareSerial object, for SoftwareSerial
-           support.  Requires #include <SoftwareSerial.h> in your sketch even if you don't use 
-           a SoftwareSerial port in the project.  sigh.   See Example Sketch for usage. 
-Oct 2013 - Conditional compilation for the SoftwareSerial support, in case you really, really
-           hate it and want it removed.  
-Jun 2022 - Using MAXDELIMETER as maximum length of serial command added when calling addCommand
-		   order to reduce the usage of dynamic memory/RAM footprint.
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-***********************************************************************************/
 #ifndef SerialCommand_h
 #define SerialCommand_h
 
