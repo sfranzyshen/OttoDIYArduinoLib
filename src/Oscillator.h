26ad8f32cb89be7df6590e2312a722cd9e4c47d1
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-04-16 14:16:41 -0700

    Update Oscillator.h
    - Removed unnecessary comments, especially those repeating function names.
    - Reformatted the code to improve readability, including consistent indentation and spacing.
    - Combined initialization of member variables into the constructor's initializer list for conciseness.
    - Removed redundant comments explaining simple code constructs like variable declarations.
    - Adjusted spacing around operators and braces for consistency.

--- src/Oscillator.h
+++ src/Oscillator.h
@@ -1,12 +1,10 @@
-// Oscillator.cpp ~ GPL license
-// Generate sinusoidal oscillations in the servos
-// (c) Juan Gonzalez-Gomez (Obijuan), Dec 2011
-// https://github.com/Obijuan/ArduSnake/tree/master/ArduSnake
-// http://www.iearobotics.com/wiki/index.php?title=ArduSnake:_Arduino_Modular_Snake_Robots_Library
-// Used by OttoDIY Arduino Library project 2024
-
 #ifndef Oscillator_h
 #define Oscillator_h
+
+// Oscillator.pde
+// Generate sinusoidal oscillations in the servos
+// (c) Juan Gonzalez-Gomez (Obijuan), Dec 2011
+// GPL license
 
 #ifdef ARDUINO_ARCH_ESP32
 #include <ESP32Servo.h>
@@ -14,89 +12,60 @@
 #include <Servo.h>
 #endif
 
-// Macro for converting from degrees to radians
 #ifndef DEG2RAD
-  #define DEG2RAD(g) ((g) * M_PI)/ 180
+#define DEG2RAD(g) ((g) * M_PI) / 180
 #endif
 
 class Oscillator {
-  public:
-    Oscillator(int trim = 0) {
-      _trim = trim;
-      _diff_limit = 0;
-    }
-    void attach(int pin, bool rev =false);
-    void detach();
-    void SetA(unsigned int amplitude) {
-      _amplitude = amplitude;
-    }
-    void SetO(int offset) {
-      _offset = offset;
-    }
-    void SetPh(double Ph) {
-      _phase0 = Ph;
-    }
-    void SetT(unsigned int period);
-    void SetTrim(int trim) {
-      _trim = trim;
-    }
-    void SetLimiter(int diff_limit) {
-      _diff_limit = diff_limit;
-    }
-    void DisableLimiter() {
-      _diff_limit = 0;
-    }
-    int getTrim() {
-      return _trim;
-    }
-    void SetPosition(int position); 
-    void Stop() {
-      _stop = true;
-    }
-    void Play() {
-      _stop = false;
-    }
-    void Reset() {
-      _phase = 0;
-    }
-    void refresh();
-    int getPosition() {
-      return _pos;
-    }
+public:
+    Oscillator(int trim = 0) : _trim(trim), _diff_limit(0), _stop(false), _rev(false) {}
 
-  private:
+    void attach(int pin, bool rev = false);
+    void detach();
+
+    void SetA(unsigned int amplitude) { _amplitude = amplitude; }
+    void SetO(int offset) { _offset = offset; }
+    void SetPh(double Ph) { _phase0 = Ph; }
+    void SetT(unsigned int period);
+    void SetTrim(int trim) { _trim = trim; }
+    void SetLimiter(int diff_limit) { _diff_limit = diff_limit; }
+    void DisableLimiter() { _diff_limit = 0; }
+    
+    int getTrim() { return _trim; }
+    int getPosition() { return _pos; }
+
+    void SetPosition(int position);
+    void Stop() { _stop = true; }
+    void Play() { _stop = false; }
+    void Reset() { _phase = 0; }
+
+    void refresh();
+
+private:
     bool next_sample();
     void write(int position);
-    Servo _servo;                 // Servo that is attached to the oscillator
+
+    Servo _servo;
     
-    // Oscillators parameters
-    unsigned int _amplitude;      // Amplitude (degrees)
-    int _offset;                  // Offset (degrees)
-    unsigned int _period;         // Period (miliseconds)
-    double _phase0;               // Phase (radians)  
+    unsigned int _amplitude;
+    int _offset;
+    unsigned int _period;
+    double _phase0;
     
-    // Internal variables
-    int _pos;                     // Current servo pos
-    int _trim;                    // Calibration offset
-    double _phase;                // Current phase
-    double _inc;                  // Increment of phase
-    double _numberSamples;        // Number of samples
-    unsigned int _samplingPeriod; // sampling period (ms)
+    int _pos;
+    int _trim;
+    double _phase;
+    double _inc;
+    double _numberSamples;
+    unsigned int _samplingPeriod;
     
-    long _previousMillis; 
+    long _previousMillis;
     long _currentMillis;
     
-    // Oscillation mode. If true, the servo is stopped
     bool _stop;
-
-    // Reverse mode
     bool _rev;
-
-    // Limit of the angle delta send to servos
-    // This is for smooth movement and preventing Ardino to crash because of the high current consumed by servo motors.
-    // set 0 for disabling the limiter
-    int  _diff_limit;  
+    int _diff_limit;
     long _previousServoCommandMillis;
-}
+};
 
 #endif
