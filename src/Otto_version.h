15700b83bef7bd82503923e3c26335dacd132e5b
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-04-26 16:09:24 -0700

    Add files via upload
    add version header

---
+++ src/Otto_version.h
@@ -0,0 +1,11 @@
+// OttoDIY Arduino Library project 2024
+
+#ifndef Otto_version_h
+#define Otto_version_h
+
+#define OTTO_VER_MAJOR    13
+#define OTTO_VER_RELEASE  1
+#define OTTO_VER_MINOR    0
+#define OTTO_VER          (OTTO_VER_MAJOR##.##OTTO_VER_RELEASE##.##OTTO_VER_MINOR)
+
+#endif // Otto_version.h
