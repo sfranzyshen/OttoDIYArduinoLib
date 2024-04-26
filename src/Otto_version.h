5569df15994ca6c3e15b9739364b7840adb384b9
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-04-26 16:10:03 -0700

    Update Otto_version.h

--- src/Otto_version.h
+++ src/Otto_version.h
@@ -4,8 +4,8 @@
 #define Otto_version_h
 
 #define OTTO_VER_MAJOR    13
-#define OTTO_VER_RELEASE  1
-#define OTTO_VER_MINOR    0
+#define OTTO_VER_RELEASE  0
+#define OTTO_VER_MINOR    1
 #define OTTO_VER          (OTTO_VER_MAJOR##.##OTTO_VER_RELEASE##.##OTTO_VER_MINOR)
 
 #endif // Otto_version.h
