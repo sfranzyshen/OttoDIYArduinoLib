da97e7ddb535fdf8e16ede72d1f688f9ee33d38a
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-05-10 10:09:13 -0700

    Update Otto_sound.h
    more sound

--- src/Otto_sound.h
+++ src/Otto_sound.h
@@ -3,7 +3,7 @@
 #ifndef Otto_sound_h
 #define Otto_sound_h
 
-#define BUZZER            0
-#define MP3_PLAYER_UART   1
+#define SOUND_BUZZER            0
+#define SOUND_MP3_PLAYER_UART   1
 
 #endif
