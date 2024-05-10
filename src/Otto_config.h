5001891b4c212fceec0a3e4cc5c07006e28af48c
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-05-10 10:11:35 -0700

    Update Otto_config.h

--- src/Otto_config.h
+++ src/Otto_config.h
@@ -3,8 +3,8 @@
 #ifndef Otto_config_h
 #define Otto_config_h
 
-#define BLOCKING		false
-#define NON_BLOCKING	true
+#define BLOCKING      false
+#define NON_BLOCKING  true
 
 #include "Otto_models.h"
 #include "Otto_sound.h"
